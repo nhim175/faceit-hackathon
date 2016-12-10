72714baf358aec4e0ddf8e8f6553a47e1ef3c78e
klinkigt <klinkigt@googlemail.com>
2016-12-10 16:33:27 +0800

    fixing classifier

--- .backend/make_classifier.py
+++ .backend/make_classifier.py
@@ -9,10 +9,15 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.naive_bayes import GaussianNB
 
 import numpy as np
+import cv2
+import openface
+import pickle
 
+import lasagne
+import theano
+import theano.tensor as T
 
 def getRep(imgPath, align, net, multiple=False):
-    start = time.time()
     bgrImg = cv2.imread(imgPath)
     if bgrImg is None:
         raise Exception("Unable to load image: {}".format(imgPath))
@@ -26,50 +31,39 @@ def getRep(imgPath, align, net, multiple=False):
         bbs = [bb1]
     if len(bbs) == 0 or (not multiple and bb1 is None):
         raise Exception("Unable to find a face: {}".format(imgPath))
-    if args.verbose:
-        print("Face detection took {} seconds.".format(time.time() - start))
 
     reps = []
     for bb in bbs:
-        start = time.time()
         alignedFace = align.align(
-            args.imgDim,
+            96,
             rgbImg,
             bb,
             landmarkIndices=openface.AlignDlib.OUTER_EYES_AND_NOSE)
         if alignedFace is None:
             raise Exception("Unable to align image: {}".format(imgPath))
-        if args.verbose:
-            print("Alignment took {} seconds.".format(time.time() - start))
-            print("This bbox is centered at {}, {}".format(bb.center().x, bb.center().y))
 
         rep = net.forward(alignedFace)
-        if args.verbose:
-            print("Neural network forward pass took {} seconds.".format(
-                time.time() - start))
-        reps.append((bb.center().x, rep))
-    sreps = sorted(reps, key=lambda x: x[0])
+        reps.append(rep)
+    sreps = np.array(reps)
+    #print(sreps.shape)
     return sreps
 
 
 def train(features, labels):
-    
-    from nolearn.dbn import DBN
-    clf = DBN([len(features), 200, len(labels),
-              learn_rates=0.3,
-              # Smaller steps mean a possibly more accurate result, but the
-              # training will take longer
-              learn_rate_decays=0.9,
-              # a factor the initial learning rate will be multiplied by
-              # after each iteration of the training
-              epochs=300,  # no of iternation
-              # dropouts = 0.25, # Express the percentage of nodes that
-              # will be randomly dropped as a decimal.
-              verbose=1)
+    le = LabelEncoder().fit(labels)
+    labelsNum = le.transform(labels)
+    nClasses = len(le.classes_)
+    # create Theano variables for input and target minibatch
+    param_grid = [
+        {'C': [1, 10, 100, 1000],
+         'kernel': ['linear']},
+        {'C': [1, 10, 100, 1000],
+         'gamma': [0.001, 0.0001],
+         'kernel': ['rbf']}
+    ]
+    clf = GridSearchCV(SVC(C=1, probability=True), param_grid, cv=5)
 
-    clf.fit(features, labels)
-
-    fName = "{}/dbn.pkl".format(".")
+    fName = "{}/svm.pkl".format(".")
     print("Saving classifier to '{}'".format(fName))
     with open(fName, 'w') as f:
         pickle.dump((le, clf), f)
@@ -79,8 +73,8 @@ def makeClassifier(allfacesdir, align, net):
     facesdir = [o for o in os.listdir(allfacesdir) if os.path.isdir(os.path.join(allfacesdir,o))]
     id_counter = 0
     
-    features = None
-    labels = None
+    features = []
+    labels = []
     
     for facedir in facesdir:
         name = facedir
@@ -90,23 +84,16 @@ def makeClassifier(allfacesdir, align, net):
         for facesdir_image in facesdir_images:
             facesdir_image_path = os.path.join(facedir_path, facesdir_image)
             print facesdir_image_path
-            reps = self.getRep(facesdir_image_path, align, net)
+            reps = getRep(facesdir_image_path, align, net)
             
-            print reps
-            
-            if features == None:
-                features = reps
-            else:
-                np.append([features, reps], axis=0)
-                
-            if labels == None:
-                labels = id_counter
-                np.append([labels, reps], axis=0)
+            features.append(reps)
+            labels.append(id_counter)
                 
                 
         id_counter += 1
     
-    self.train(features, labels)
+    print features
+    train(features, labels)
 
 if __name__ == "__main__":
     dlib_predictor = "./.resources/shape_predictor_68_face_landmarks.dat"
@@ -114,4 +101,4 @@ if __name__ == "__main__":
     network_model = "./.resources/nn4.small2.v1.t7"
     net = openface.TorchNeuralNet(network_model, 96)
     
-    self.makeClassifier("./.resources/")
+    makeClassifier("./.resources/", align, net)
