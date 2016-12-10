b14d3c999b019ad248ddb9ff4b59d39beee7b203
Tonny Pham <jwall149@gmail.com>
2016-12-10 17:35:24 +0900

    Update deploy script

--- .meteor/bin/deploy.sh
+++ .meteor/bin/deploy.sh
@@ -3,11 +3,13 @@
 # Cleanup
 
 rm -rf .meteor/local/*
-ssh root@47.91.16.100 rm -rf /www/faceit/*
 
 # Build project
+npm install
 meteor build --directory .meteor/local --server-only --server http://faceit.tonny.me
 
-cp .meteor/.bin/start.sh .meteor/local/bundle/
+cd .meteor/local/bundle/programs/server
+npm install
 
-scp -r .meteor/local/bundle/* root@47.91.16.100:/www/faceit
+cd ../../
+node main.js
