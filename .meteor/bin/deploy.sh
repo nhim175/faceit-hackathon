d374942762b584be01d4a7a12a087b8f805d98ea
Tonny Pham <jwall149@gmail.com>
2016-12-10 16:43:50 +0900

    Add deploy script

--- .meteor/bin/deploy.sh
+++ .meteor/bin/deploy.sh
@@ -8,8 +8,6 @@ ssh root@47.91.16.100 rm -rf /www/faceit/*
 # Build project
 meteor build --directory .meteor/local --server-only --server http://faceit.tonny.me
 
+cp .meteor/.bin/start.sh .meteor/local/bundle/
+
 scp -r .meteor/local/bundle/* root@47.91.16.100:/www/faceit
-
-# scp .meteor/bin/remote_start.sh root@47.91.16.100:/www
-
-# ssh
