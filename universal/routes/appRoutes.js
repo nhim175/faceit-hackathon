b4008f5fa1b9fafb43974fb9e1fff8e2c5a33642
Thinh Pham <nhim175@gmail.com>
2016-12-11 09:26:56 +0900

    (refactor) set / route to selectApp

--- universal/routes/appRoutes.js
+++ universal/routes/appRoutes.js
@@ -2,7 +2,7 @@ import { renderBasic, renderSlim } from './helpers.js';
 
 // TODO: call this in entry file
 export default function (FlowRouter) {
-  FlowRouter.route('/select-app', {
+  FlowRouter.route('/', {
     action: () => renderSlim('selectApp')
   });
 }
--- universal/routes/mainRoutes.js
+++ universal/routes/mainRoutes.js
@@ -1,10 +1,6 @@
 import { renderBasic, renderSlim } from './helpers.js';
 
 export default function (FlowRouter) {
-  FlowRouter.route('/', {
-    action: () => renderBasic('home')
-  });
-
   FlowRouter.notFound = {
     action: () => renderSlim('notFound')
   };
