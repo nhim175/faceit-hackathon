91189e149982e55dbf9968f652dc087342a83f3a
Thinh Pham <nhim175@gmail.com>
2016-12-11 15:02:29 +0900

    Clean up frontend code

--- client/components/selectApp/selectApp.js
+++
@@ -1,8 +0,0 @@
-// TODO: call this in entry file
-export default function (Template) {
-  Template['selectApp'].helpers({
-  });
-
-  Template['selectApp'].events({
-  });
-}
--- universal/routes/appRoutes.js
+++
@@ -1,8 +0,0 @@
-import { renderBasic, renderSlim } from './helpers.js';
-
-// TODO: call this in entry file
-export default function (FlowRouter) {
-  FlowRouter.route('/select-app', {
-    action: () => renderBasic('selectApp')
-  });
-}
